9cf56a974dd63348947902c42dd2f784432c6bef
unicrypto11 <ccrypto1992@gmail.com>
2024-10-07 22:09:30 -0500

    feat: init_donate func
    Co-authored-by: unicrypto11 <9045652+unicrypto11@users.noreply.github.com>
    Co-authored-by: hcrypto7 <153700650+hcrypto7@users.noreply.github.com>

--- tests/soonak_memes_program.ts
+++ tests/soonak_memes_program.ts
@@ -16,10 +16,13 @@ describe("soonak_memes_program", () => {
 
   it("Is Comp Created!", async () => {
     // Add your test here.
-    const [compPda] = await anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("comp")], program.programId);
-    const tx = await program.methods.createComp().rpc();
+    const [compPda] = await anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("comp"), new anchor.web3.PublicKey("GqPZ3xwsZqbuq76VwFv6i4N2QKuU4bdFYRrgMrehwmJf").toBuffer()], program.programId);
+    const [prizePoolPda] = await anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("prize_pool"), new anchor.web3.PublicKey("GqPZ3xwsZqbuq76VwFv6i4N2QKuU4bdFYRrgMrehwmJf").toBuffer()], program.programId);
+    const tx = await program.methods.createComp().accounts({ tokenAddress: "GqPZ3xwsZqbuq76VwFv6i4N2QKuU4bdFYRrgMrehwmJf" }).rpc();
     const comp = await program.account.comp.fetch(compPda);
-    console.log("comp", comp);
+    const prize_pool = await program.account.prizePool.fetch(prizePoolPda);
+    console.log("prize_pool:", prize_pool.totalAmount.toString());
+    console.log("comp create time:", new Date(comp.createTime.toNumber() * 1000).toUTCString());
     console.log("Your transaction signature", tx);
   });
 });
